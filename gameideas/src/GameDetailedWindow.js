9478e023f2b0f8b6b5807d5245e9f344a1e0745b
Joakim Svensson <joakimjlsv@gmail.com>
2022-07-02 21:40:28 +0100

    onClick for Gamewindow now in app and passes number

---
+++ gameideas/src/GameDetailedWindow.js
@@ -0,0 +1,11 @@
+import './App.css';
+import React from 'react';
+
+function GameDetailedWindow(props) {
+    return(
+    <div className='GameDetailedWindow'>
+        I am opened
+    </div>);
+}
+
+export default GameDetailedWindow;
